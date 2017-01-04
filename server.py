031c5f1dcef4ed99618f556e4c81c2643c477d20
Jessica Langston <jessica.langston11@gmail.com>
2017-01-04 04:40:10 -0800

    Define port for Heroku deployment

--- server.py
+++ server.py
@@ -62,4 +62,5 @@ def handle_response(resp):
 
 # run the local server with this Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
